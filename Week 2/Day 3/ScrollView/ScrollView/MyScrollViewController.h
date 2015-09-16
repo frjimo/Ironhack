5030393c05a2fdd1f338d40c1c7568cd7c90c35c
Fran <frjimo@gmail.com>
2015-09-16 12:55:28 +0200

    Week 2. Day 3. UIScrollView

---
+++ Week 2/Day 3/ScrollView/ScrollView/MyScrollViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyScrollViewController.h
+//  ScrollView
+//
+//  Created by Fran on 16/9/15.
+//  Copyright (c) 2015 Fran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyScrollViewController : UIViewController
+
+@end
---
+++ Week 2/Day 3/ScrollView/ScrollView/MyScrollViewController.m
@@ -0,0 +1,37 @@
+//
+//  MyScrollViewController.m
+//  ScrollView
+//
+//  Created by Fran on 16/9/15.
+//  Copyright (c) 2015 Fran. All rights reserved.
+//
+
+#import "MyScrollViewController.h"
+
+@interface MyScrollViewController ()
+
+@end
+
+@implementation MyScrollViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
