d59d674d71cf8e40b634cc0a542ae9c1c31840d3
Fran <frjimo@gmail.com>
2015-09-22 12:26:20 +0200

    Week 3. Day 2. AutoLayout

---
+++ Week 3/Day 2/AutoLayout/AutoLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayout
+//
+//  Created by Fran on 22/9/15.
+//  Copyright (c) 2015 Fran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Week 3/Day 2/AutoLayout/AutoLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayout
+//
+//  Created by Fran on 22/9/15.
+//  Copyright (c) 2015 Fran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
