d3416e6f4ffaafec23491a6e132d978a5930fb00
Fran <frjimo@gmail.com>
2015-09-25 09:21:05 +0200

    Week 3. Day 4. BasicAnimation

--- Week 3/Day 4/BasicAnimations/BasicAnimations/ViewController.m
+++ Week 3/Day 4/BasicAnimations/BasicAnimations/ViewController.m
@@ -9,19 +9,64 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIView *imageView;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (CABasicAnimation *)animationX {
+    CABasicAnimation *basicAnimation = [CABasicAnimation animationWithKeyPath:@"position.x"];
+    basicAnimation.toValue = @0;
+//    basicAnimation.duration = 5;
+//    basicAnimation.fillMode = kCAFillModeForwards;
+//    basicAnimation.removedOnCompletion = NO;
+    
+//    [self.imageView.layer addAnimation:basicAnimation forKey:@"opacity1Animation"];
+    return basicAnimation;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (CABasicAnimation *)animationY {
+    CABasicAnimation *basicAnimation = [CABasicAnimation animationWithKeyPath:@"position.y"];
+    basicAnimation.toValue = @50;
+//    basicAnimation.duration = 5;
+//    basicAnimation.fillMode = kCAFillModeForwards;
+//    basicAnimation.removedOnCompletion = NO;
+    
+//    [self.imageView.layer addAnimation:basicAnimation forKey:@"opacity2Animation"];
+    return basicAnimation;
 }
+
+- (CABasicAnimation *)animationOpaque {
+    CABasicAnimation *basicAnimation = [CABasicAnimation animationWithKeyPath:@"opacity"];
+    basicAnimation.toValue = @0.1;
+    basicAnimation.duration = 3;
+    basicAnimation.fillMode = kCAFillModeForwards;
+//    basicAnimation.removedOnCompletion = NO;
+    
+//    [self.imageView.layer addAnimation:basicAnimation forKey:@"opacity3Animation"];
+    return basicAnimation;
+}
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+//    [self animationX];
+//    [self animationY];
+//    [self animationOpaque];
+    
+    
+    CAAnimationGroup *groupAnimation = [CAAnimationGroup animation];
+    groupAnimation.animations = [NSArray arrayWithObjects:[self animationX], [self animationY], [self animationOpaque], nil];
+    groupAnimation.duration = 5;
+    groupAnimation.fillMode = kCAFillModeForwards;
+    groupAnimation.removedOnCompletion = NO;
+    
+    [self.imageView.layer addAnimation:groupAnimation forKey:@"groupAnimation"];
+
+    
+}
+
 
 @end
