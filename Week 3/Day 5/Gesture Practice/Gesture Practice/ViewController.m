9bd66b4c319b03f44aabfb8ea0e4672c73cc1952
Fran <frjimo@gmail.com>
2015-09-25 11:18:34 +0200

    Week 3. Day 5. UIPinchGestureRecognizer

--- Week 3/Day 5/Gesture Practice/Gesture Practice/ViewController.m
+++ Week 3/Day 5/Gesture Practice/Gesture Practice/ViewController.m
@@ -21,12 +21,45 @@
     UITapGestureRecognizer *tapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTapGesture:)];
     tapRecognizer.numberOfTapsRequired = 1;
     [self.view addGestureRecognizer:tapRecognizer];
+    
+    
+    
+    UIPinchGestureRecognizer *pinchRecognizer = [[UIPinchGestureRecognizer alloc]initWithTarget:self action:@selector(handlePinchGesture:)];
+    [self.view addGestureRecognizer:pinchRecognizer];
 }
 
-- (void)handleTapGesture:(UITapGestureRecognizer *)tapGesture {
-    [self createImage:[tapGesture locationInView:self.view]];
-    
+
+- (void)handlePinchGesture:(UIPinchGestureRecognizer *)pinchGestureRecognizer {
+    UIView *imageView = [self imageViewBehindGestureRecognizerIfAny:pinchGestureRecognizer];
+    if(!imageView || pinchGestureRecognizer.state != UIGestureRecognizerStateChanged) {
+        return;
+    }
+    imageView.transform = CGAffineTransformScale(imageView.transform, pinchGestureRecognizer.scale, pinchGestureRecognizer.scale);
+    pinchGestureRecognizer.scale = 1;
 }
+
+- (UIView *)imageViewBehindGestureRecognizerIfAny:(UIPinchGestureRecognizer *)gestureRecognizer {
+    CGPoint point = [gestureRecognizer locationInView:self.view];
+    UIView *imageView = [self.view hitTest:point withEvent:nil];
+    if(![imageView isKindOfClass: [UIImageView class]]){
+        return nil;
+    }
+    return (UIImageView *)imageView;
+}
+
+
+- (void)handleTapGesture:(UITapGestureRecognizer *)tapGestureRecognizer {
+    [self createImage:[tapGestureRecognizer locationInView:self.view]];
+}
+
+
+
+
+
+
+
+
+
 
 - (void)createImage:(CGPoint )location {
     CGRect frame = CGRectMake(0, 0, 200, 200);
@@ -36,6 +69,7 @@
     imageView.image = [UIImage imageNamed:@"vaderCat"];
     
     imageView.center =location;
+    imageView.userInteractionEnabled = YES;
     [self.view addSubview:imageView];
     
 }
