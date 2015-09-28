419339b2df0caee411cdb59e69816a85923616ec
Fran <frjimo@gmail.com>
2015-09-28 15:39:23 +0200

    Week 4. Day 1. Bookmark -isEqual Refactor

--- Week 4/Day 1/2_Bookmarks/Bookmarks/Bookmark.m
+++ Week 4/Day 1/2_Bookmarks/Bookmarks/Bookmark.m
@@ -27,7 +27,8 @@
 }
 
 - (BOOL)isEqualBookmark:(Bookmark *)other {
-    return ([self.name isEqualToString:other.name] && [self.url isEqualToString: other.url]);
+    //return ([self.name isEqualToString:other.name] && [self.url isEqualToString: other.url]);
+    return ([self.url isEqualToString: other.url]);
 }
 
 
@@ -44,10 +45,11 @@
 
 - (NSUInteger)hash
 {
-    NSLog(@"url:%lu",[_url hash]);
-    NSLog(@"name:%lu",[_name hash]);
-    NSLog(@"Valor: %lu",[_url hash] ^ [_name hash]);
-    return [_url hash] ^ [_name hash];
+//    NSLog(@"url:%lu",[_url hash]);
+//    NSLog(@"name:%lu",[_name hash]);
+//    NSLog(@"Valor: %lu",[_url hash] ^ [_name hash]);
+//    return [_url hash] ^ [_name hash];
+    return [_url hash];
 }
 
 @end
