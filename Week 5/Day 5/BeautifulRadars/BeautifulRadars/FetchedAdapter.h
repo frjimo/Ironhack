72a8e42071aaabe33a251010faa4aed174fb848a
Fran <frjimo@gmail.com>
2015-10-12 11:38:56 +0200

    Week 5. Day 5 BeautifulRadars

---
+++ Week 5/Day 5/BeautifulRadars/BeautifulRadars/FetchedAdapter.h
@@ -0,0 +1,14 @@
+//
+//  FetchedAdapter.h
+//  BeautifulRadars
+//
+//  Created by Fran on 9/10/15.
+//  Copyright © 2015 Diego Freniche Brito. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "CoreDataStack.h"
+
+@interface FetchedAdapter : NSObject <NSFetchedResultsControllerDelegate>
+
+@end
